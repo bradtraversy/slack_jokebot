b5c79bffc844200090796f79a48d85013deefe38
Brad Traversy <traversymedia@gmail.com>
2018-06-04 17:07:01 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# @Jokebot
+
+> Simple slackbot built with Node.js and Slackbots.js that tells random Chuck Norris and Yo Mama jokes.
+
+## APIs Used:
+[http://www.icndb.com/api/](http://www.icndb.com/api/)
+[http://yomomma.info/](http://yomomma.info/)
+
+
+## Quick Start
+
+``` bash
+# Install dependencies
+npm install
+
+# Serve on localhost:3000
+npm start
+
+# Create bot in Slack and generate and include your OAuth bot token
+```
+
+## App Info
+
+### Author
+
+Brad Traversy
+[Traversy Media](http://www.traversymedia.com)
+
+### Version
+
+1.0.0
+
+### License
+
+This project is licensed under the MIT License
---
+++ index.js
@@ -0,0 +1,94 @@
+const SlackBot = require('slackbots');
+const axios = require('axios');
+
+const bot = new SlackBot({
+  token: 'xoxb-YOUR-OWN-TOKEN',
+  name: 'jokebot'
+});
+
+// Start Handler
+bot.on('start', () => {
+  const params = {
+    icon_emoji: ':smiley:'
+  };
+
+  bot.postMessageToChannel(
+    'general',
+    'Get Ready To Laugh With @Jokebot!',
+    params
+  );
+});
+
+// Error Handler
+bot.on('error', err => console.log(err));
+
+// Message Handler
+bot.on('message', data => {
+  if (data.type !== 'message') {
+    return;
+  }
+
+  handleMessage(data.text);
+});
+
+// Respons to Data
+function handleMessage(message) {
+  if (message.includes(' chucknorris')) {
+    chuckJoke();
+  } else if (message.includes(' yomama')) {
+    yoMamaJoke();
+  } else if (message.includes(' random')) {
+    randomJoke();
+  } else if (message.includes(' help')) {
+    runHelp();
+  }
+}
+
+// Tell a Chuck Norris Joke
+function chuckJoke() {
+  axios.get('http://api.icndb.com/jokes/random').then(res => {
+    const joke = res.data.value.joke;
+
+    const params = {
+      icon_emoji: ':laughing:'
+    };
+
+    bot.postMessageToChannel('general', `Chuck Norris: ${joke}`, params);
+  });
+}
+
+// Tell a Yo Mama Joke
+function yoMamaJoke() {
+  axios.get('http://api.yomomma.info').then(res => {
+    const joke = res.data.joke;
+
+    const params = {
+      icon_emoji: ':laughing:'
+    };
+
+    bot.postMessageToChannel('general', `Yo Mama: ${joke}`, params);
+  });
+}
+
+// Tell a Random Joke
+function randomJoke() {
+  const rand = Math.floor(Math.random() * 2) + 1;
+  if (rand === 1) {
+    chuckJoke();
+  } else if (rand === 2) {
+    yoMamaJoke();
+  }
+}
+
+// Show Help Text
+function runHelp() {
+  const params = {
+    icon_emoji: ':question:'
+  };
+
+  bot.postMessageToChannel(
+    'general',
+    `Type @jokebot with either 'chucknorris', 'yomama' or 'random' to get a joke`,
+    params
+  );
+}
